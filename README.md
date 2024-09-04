f7a56e1af8220879ef2832fbf4dc4dae07abeccf
Shani Staretz <shakimi88@gmail.com>
2024-09-04 19:05:12 +0300

    elif- definition and usage

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 in the lecture:
 1) recap last lecture and solved hw2.
 2) elif
+3) "and" vs & : & =bitwise AND (working on the number in binary)
---
+++ lec3.py
@@ -0,0 +1,10 @@
+x: float = float(input("enter x:\n"));
+if x > 0:
+    # yes
+    print(x, "is positive")
+elif x == 0:
+    # yes
+    print(x, "is zero")
+else:
+    # everything was no
+    print(x, "is negative")
